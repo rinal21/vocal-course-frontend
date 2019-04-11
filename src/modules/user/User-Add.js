8422919cc6fb8c64fe9179e5d203a487bb7f6b6e
rinal21 <lawliet148@yahoo.com>
2019-04-11 07:37:20 +0700

    Create crud form module user

---
+++ src/modules/user/User-Add.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import UserAddForm from "./form/Add-Form"
+
+
+export default class studentList extends Component {
+    render() {
+        return(
+            <div>
+                <div className="box-header with-border">
+                    <h3>
+                        Add User
+                      </h3>
+                </div>
+                <div className="box-body">
+                    <UserAddForm />
+                </div>
+            </div>
+        )
+    }
+}
+
