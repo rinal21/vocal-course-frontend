0b41425acd443d5c190b4ce39b86c291e0131307
rinal21 <lawliet148@yahoo.com>
2019-04-26 15:19:34 +0700

    Add crud teacher-attendances

--- src/modules/teacher-attendance/Teacher-List.js
+++ src/modules/teacher-attendance/Teacher-List.js
@@ -1,259 +1,154 @@
 import React, { Component } from "react";
-import { MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBDataTable  } from 'mdbreact';
+import { MDBDataTable  } from 'mdbreact';
 import { NavLink } from "react-router-dom";
-
-const data = {
-    columns: [
-      {
-        label: 'Name',
-        field: 'name',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Address',
-        field: 'address',
-        sort: 'asc',
-        width: 270
-      },
-      {
-        label: 'School',
-        field: 'school',
-        sort: 'asc',
-        width: 200
-      },
-      {
-        label: 'Email',
-        field: 'email',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Birth date',
-        field: 'birthDate',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Age',
-        field: 'age',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Sex',
-        field: 'sex',
-        width: 100
-      },
-      {
-        label: 'Phone',
-        field: 'phone',
-        width: 100
-      },
-      {
-        label: 'Edit',
-        field: 'edit',
-        width: 100
-      },
-      {
-        label: 'Delete',
-        field: 'delete',
-        width: 100
-      }
-    ],
-    rows: [
-      {
-        name: 'Andi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Budi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Charly',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Andi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Budi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Charly',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },{
-        name: 'Andi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Budi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Charly',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },{
-        name: 'Andi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Budi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Charly',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },{
-        name: 'Andi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Budi',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-      {
-        name: 'Charly',
-        address: 'Jln Mawar Raya No. 9',
-        school: 'Edinburgh',
-        email: 'email@email.com',
-        birthDate: '2011/04/25',
-        age: '17',
-        sex: 'M',
-        phone: '0987654321',
-        edit: <button className="btn btn-primary">Edit</button>,
-        delete: <button className="btn btn-danger">Delete</button>
-      },
-    ]
-  };
+import moment from "moment";
+import DatePicker from "react-datepicker";
 
 export default class teacherList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      teachers: [],
+      filterDate: new Date()
+    }
+    // this.delete = this.delete.bind(this);
+  }
+
+  onChangeFilterDate = filterDate => {
+    this.setState({
+      filterDate: filterDate
+    });
+    this.filterData(filterDate)
+  }
+
+  // delete(id) {
+  //   axios.delete('http://localhost:8000/api/teacher/' + id)
+  //     .then(console.log('Deleted'))
+  //     .then(() => this.fetchData())
+  //     .catch(err => console.log(err))
+  // }
+
+  componentDidMount = () => {
+    // ajax call
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    fetch('http://localhost:8000/api/teachersAttend')
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
+          teachers: json
+        })
+      })
+  }
+
+  filterData = (filterDate) => {
+    fetch('http://localhost:8000/api/teachersAttend/filter?date='+moment(filterDate).format("YYYY-MM-DD"))
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
+          teachers: json
+        })
+      })
+  }
+
+  data = (teachers) => {
+    const checklist = this.checklistIcon
+
+    return ({
+      columns: [
+        {
+          label: 'Date',
+          field: 'date',
+          sort: 'asc',
+          width: 150
+        },
+        {
+          label: 'Name',
+          field: 'name',
+          sort: 'asc',
+          width: 270
+        },
+        {
+          label: 'Absent',
+          field: 'absent',
+          sort: 'asc',
+          width: 200
+        },
+        {
+          label: 'With Permission',
+          field: 'permission',
+          sort: 'asc',
+          width: 100
+        },
+        {
+          label: 'Attend',
+          field: 'attend',
+          sort: 'asc',
+          width: 150
+        },
+        {
+          label: 'Edit',
+          field: 'edit',
+          width: 100
+        },
+        // {
+        //   label: 'Delete',
+        //   field: 'delete',
+        //   width: 100
+        // }
+      ],
+      rows: (function () {
+        let rowData = []
+
+        teachers.map((data) => {
+          rowData.push({
+            date: data.date,
+            name: data.name,
+            absent: data.status == 1 && <i class="fa fa-check" style={{ float: "right", position: "relative", right: "50%" }}></i>,
+            permission: data.status == 2 && <i class="fa fa-check" style={{ float: "right", position: "relative", right: "50%" }}></i>,
+            attend: data.status == 3 && <i class="fa fa-check" style={{ float: "right", position: "relative", right: "50%" }}></i>,
+            edit: <NavLink
+              to={{
+                pathname: 'teacherAttend/edit',
+                state: {
+                  // teacherId: data.id,
+                  // name: data.name,
+                  // salary: data.salary
+                }
+              }}
+              className="btn btn-primary">Edit</NavLink>,
+            // delete: <button onClick={() => teacherDelete(data.id)} className="btn btn-danger">Delete</button>
+          })
+        })
+
+        return rowData
+      }())
+    })
+  };
     render() {
         return(
             <div>
               <div class="box-header">
                   <NavLink to="/teacher-attendance/add" class="btn btn-success"><i class="fa fa-plus"></i> Add Teacher Attendance</NavLink>
+                  <div class="float-right">
+                  <DatePicker
+                        selected={this.state.filterDate}
+                        onChange={this.onChangeFilterDate}
+                        dateFormat="d-MM-yyyy"
+                        peekNextMonth
+                        dropdownMode="select"
+                        className="form-control"
+                      />
+                  </div>
               </div>
                 <MDBDataTable
                     striped
                     bordered
                     hover
-                    data={data}
+                    data={this.data(this.state.teachers)}
                     btn
                 />
             </div>
--- src/modules/teacher-attendance/form/Add-Form.js
+++ src/modules/teacher-attendance/form/Add-Form.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
 import DatePicker from "react-datepicker";
-import ReactAutocomplete from 'react-autocomplete'
+import Select from 'react-select';
+import moment from "moment";
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -9,18 +12,98 @@ export default class userAdd extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date(),
-      value: '',
+      dateAttend: new Date(),
+      teachers: [],
+      teacherId: '',
+      selectedTeacher: null,
+      // absent: 0,
+      // vacation: 0,
+      selectedAttendance: '',
+      redirect: false
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.onChangeDateAttend = this.onChangeDateAttend.bind(this);
+    this.onChangeTeachers = this.onChangeTeachers.bind(this);
+    // this.onChangeAbsent = this.onChangeAbsent.bind(this);
+    // this.onChangeVacation = this.onChangeVacation.bind(this);
+    this.onChangeAttendance = this.onChangeAttendance.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  handleChange(date) {
+  onChangeDateAttend = dateAttend => {
     this.setState({
-      startDate: date
+      dateAttend: dateAttend
     });
   }
+
+  onChangeTeachers = (selectedTeacher) =>  {
+    this.setState({ selectedTeacher });
+    this.setState({ teacherId: selectedTeacher.value})
+  }
+  onChangeAttendance = changeEvent => {
+    this.setState({selectedAttendance: changeEvent.target.value})  
+  }
+  // onChangeAbsent = changeEvent => {
+  //   this.setState({absent: changeEvent.target.value})  
+  // }
+  // onChangeVacation = changeEvent => {
+  //   this.setState({vacation: changeEvent.target.value})  
+  // }
+  // onChangeAttend = changeEvent => {
+  //   this.setState({attend: changeEvent.target.value})  
+  // }
+
+  componentDidMount = () => {
+    // ajax call
+    this.fetchTeachers()
+  }
+
+  fetchTeachers = () => {
+    fetch('http://localhost:8000/api/teachers')
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
+          teachers: json.data
+        })
+      })
+  }
+
+  dataTeachers = (teachers) => {
+    return (
+      function () {
+        let rowData = []
+        teachers.map((data) => {
+          rowData.push({
+            value: data.id,
+            label: data.name,
+          })
+        })
+        return rowData
+      }()
+    )
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      teacherId: this.state.teacherId,
+      dateAttend: moment(this.state.dateAttend).format("YYYY-MM-DD hh:mm:ss"),
+      status: this.state.selectedAttendance,
+      // vacation: this.state.vacation,
+      // attend: this.state.attend,
+    };
+    axios.post('http://localhost:8000/api/teacherAttend', obj)
+      .then(res => console.log(res.data))
+      .then(() => this.setState({ redirect: true }))
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+
   render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to='/teacher-attendance' />;
+    }
     return (
       <div>
         <Formik
@@ -54,15 +137,16 @@ export default class userAdd extends Component {
             /* and other goodies */
           }) => (
               <div>
-                <form action="/action_page.php">
+                <form onSubmit={this.onSubmit}>
                   <div className="form-inline mb-2">
                     <label for="date" class="mr-sm-2 text-left d-block" style={{ width: 140 }}>
                       Date
                     </label>
                     <label>: &nbsp;</label>
                     <DatePicker
-                        selected={this.state.startDate}
-                        onChange={this.handleChange}
+                        selected={this.state.dateAttend}
+                        onChange={this.onChangeDateAttend}
+                        dateFormat="d-MM-yyyy"
                         peekNextMonth
                         dropdownMode="select"
                         className="form-control"
@@ -73,50 +157,55 @@ export default class userAdd extends Component {
                       Teacher Name
                   </label>
                     <label>: &nbsp;</label>
-                    <ReactAutocomplete
-                      items={[
-                        { id: '1', label: 'Andi' },
-                        { id: '2', label: 'Budi' },
-                        { id: '3', label: 'Charly' },
-                      ]}
-                      shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
-                      getItemValue={item => item.label}
-                      renderInput={props => <input {...props} className='form-control'/>}
-                      renderItem={(item, highlighted) =>
-                        <div
-                          key={item.id}
-                          style={{ backgroundColor: highlighted ? '#ddd' : 'transparent' }}
-                        >
-                          {item.label}
-                        </div>
-                      }
-                      value={this.state.value}
-                      onChange={e => this.setState({ value: e.target.value })}
-                      onSelect={value => this.setState({ value })}
-                    />
+                    <div style={{ display: 'inline-block', width: 223.2 }}>
+                      <Select
+                        value={this.state.selectedTeacher}
+                        onChange={this.onChangeTeachers}
+                        options={this.dataTeachers(this.state.teachers)}
+                      />
+                    </div>
                   </div>
                   <div className="form-inline mb-2">
                     <label for="absent" class="mr-sm-2 text-left d-block" style={{ width: 140 }}>
-                      Is Absent
+                      Attendance
                   </label>
                     <label>: &nbsp;</label>
-                    <select class="form-control">
-                      <option value="">Choose one</option>
-                      <option value="false">No</option>
-                      <option value="true">Yes</option>
-                    </select>
+                    <label class="radio-inline mr-2">
+                      <input type="radio" name="optattandance" value="1"
+                        checked={this.state.selectedAttendance === "1"}
+                        onChange={this.onChangeAttendance} />Absent
+                    </label>
+                    <label class="radio-inline mr-2"><input type="radio" name="optattandance" value="2"
+                      checked={this.state.selectedAttendance === "2"}
+                      onChange={this.onChangeAttendance} />With Permission
+                    </label>
+                    <label class="radio-inline"><input type="radio" name="optattandance" value="3"
+                      checked={this.state.selectedAttendance === "3"}
+                      onChange={this.onChangeAttendance} />Attend
+                    </label>
                   </div>
-                  <div className="form-inline mb-2">
+                  {/* <div className="form-inline mb-2">
                     <label for="permission" class="mr-sm-2 text-left d-block" style={{ width: 140 }}>
                       Is Vacation
                   </label>
                     <label>: &nbsp;</label>
-                    <select class="form-control">
-                      <option value="">Choose one</option>
-                      <option value="false">No</option>
-                      <option value="true">Yes</option>
-                    </select>
+                    <label>
+                      <input type="checkbox" name="vacation" value="1" class="checkbox"
+                        checked={this.state.vacation === "1"}
+                        onChange={this.onChangeVacation} />
+                    </label>
                   </div>
+                  <div className="form-inline mb-2">
+                    <label for="permission" class="mr-sm-2 text-left d-block" style={{ width: 140 }}>
+                      Is Attend
+                  </label>
+                    <label>: &nbsp;</label>
+                    <label>
+                      <input type="checkbox" name="attend" value="1" class="checkbox"
+                        checked={this.state.attend === "1"}
+                        onChange={this.onChangeAttend} />
+                    </label>
+                  </div> */}
                   
                   <div className="form-group">
                     <button type="submit" class="btn btn-primary mb-2">
