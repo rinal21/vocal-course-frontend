cf33675b4132b91a7fe15a300990f310f0b44716
rinal21 <lawliet148@yahoo.com>
2019-05-06 14:34:16 +0700

    Use formik in user modules

--- src/modules/user/form/Add-Form.js
+++ src/modules/user/form/Add-Form.js
@@ -1,51 +1,15 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom'
 
 export default class userAdd extends Component {
   constructor(props) {
     super(props);
-    
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
-      email: '',
-      username: '',
-      password: '',
       redirect: false
     };
-  }
-
-  onChangeEmail(e) {
-    this.setState({
-      email: e.target.value
-    });
-  }
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    })  
-  }
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value
-    })
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const obj = {
-      email: this.state.email,
-      username: this.state.username,
-      password: this.state.password
-    };
-    axios.post('http://localhost:8000/api/user', obj)
-        .then(res => console.log(res.data))
-        .then(() => this.setState({ redirect: true }));
   }
 
   render() {
@@ -53,84 +17,78 @@ export default class userAdd extends Component {
     if (redirect) {
       return <Redirect to='/user' />;
     }
+    const UserSchema = Yup.object().shape({
+      username: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    });
+
     return (
       <div>
         <Formik
-          initialValues={{ email: "", password: "" }}
-          validate={values => {
-            let errors = {};
-            if (!values.email) {
-              errors.email = "Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
-            return errors;
+          initialValues={{
+            username: "",
+            email: "",
+            password: "",
           }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
+          validationSchema={UserSchema}
+          onSubmit={values => {
+            const obj = {
+              username: values.username,
+              email: values.email,
+              password: values.password,
+            };
+            axios.post('http://localhost:8000/api/user', obj)
+              .then(res => console.log(res.data))
+              .then(() => this.setState({ redirect: true }));
+          }}>
           {({
-            values,
             errors,
             touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting
-            /* and other goodies */
           }) => (
               <div>
-                <form onSubmit={this.onSubmit}>
+                <Form>
                   <div className="form-group mb-2">
                     <label for="username" class="mr-sm-2 text-left d-block" >
                       Username
                     </label>
-                    <input 
-                    type="text" 
-                    class="form-control mr-sm-2 w-25" 
-                    id="username"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
-                     />
+                    <Field type="text" class="form-control mr-sm-2 w-25" name="username" />
+                    {errors.username && touched.username ? (
+                      <div style={{ color: 'red' }}>{errors.username}</div>
+                    ) : null}
                   </div>
                   <div className="form-group mb-2">
                     <label for="email" class="mr-sm-2 text-left d-block" >
                       Email
                     </label>
-                    <input 
-                    type="text" 
-                    class="form-control mr-sm-2 w-25" 
-                    id="email"
-                    value={this.state.email}
-                    onChange={this.onChangeEmail}
-                    />
+                    <Field type="text" class="form-control mr-sm-2 w-25" name="email" />
+                    {errors.email && touched.email ? (
+                      <div style={{ color: 'red' }}>{errors.email}</div>
+                    ) : null}
                   </div>
                   <div className="form-group mb-2">
                     <label for="password" class="mr-sm-2 text-left d-block" >
                       Password
                     </label>
-                    <input 
-                    type="password" 
-                    class="form-control mr-sm-2 w-25" 
-                    id="password"
-                    value={this.state.password}
-                    onChange={this.onChangePassword}
-                    />
+                    <Field type="password" class="form-control mr-sm-2 w-25" name="password" />
+                    {errors.password && touched.password ? (
+                      <div style={{ color: 'red' }}>{errors.password}</div>
+                    ) : null}
                   </div>
-                  
-
                   <div className="form-group">
                     <button type="submit" class="btn btn-primary mb-2">
                       Submit
                     </button>
                   </div>
-                </form>
+                </Form>
               </div>
             )}
         </Formik>
--- src/modules/user/form/Edit-Form.js
+++ src/modules/user/form/Edit-Form.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom'
 
 export default class userEdit extends Component {
   constructor(props) {
-    const { email, username } = props
     super(props);
-    
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
-      email: email,
-      username: username,
-      password: '',
       redirect: false
     };
   }
@@ -54,72 +45,79 @@ export default class userEdit extends Component {
     if (redirect) {
       return <Redirect to='/user' />;
     }
+    const UserSchema = Yup.object().shape({
+      username: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    });
     
+    const { email, username, userId } = this.props
     return (
       <div>
         <Formik
-          initialValues={{ email: "", password: "" }}
-          validate={values => {
-            let errors = {};
-            if (!values.email) {
-              errors.email = "Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
-            return errors;
+          initialValues={{
+            username: username,
+            email: email,
+            password: "",
           }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
+          validationSchema={UserSchema}
+          onSubmit={values => {
+            const obj = {
+              username: values.username,
+              email: values.email,
+              password: values.password,
+            };
+            axios.patch('http://localhost:8000/api/user/'+userId, obj)
+              .then(res => console.log(res.data))
+              .then(() => this.setState({ redirect: true }));
+          }}>
           {({
-            values,
             errors,
             touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting
-            /* and other goodies */
           }) => (
               <div>
-                <form onSubmit={this.onSubmit}>
+                <Form>
                   <div className="form-group mb-2">
                     <label for="username" class="mr-sm-2 text-left d-block" >
                       Username
                     </label>
-                    <input 
-                    type="text" 
-                    class="form-control mr-sm-2 w-25" 
-                    id="username"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
-                     />
+                    <Field type="text" class="form-control mr-sm-2 w-25" name="username" />
+                    {errors.username && touched.username ? (
+                      <div style={{ color: 'red' }}>{errors.username}</div>
+                    ) : null}
                   </div>
                   <div className="form-group mb-2">
                     <label for="email" class="mr-sm-2 text-left d-block" >
                       Email
                     </label>
-                    <input 
-                    type="text" 
-                    class="form-control mr-sm-2 w-25" 
-                    id="email"
-                    value={this.state.email}
-                    onChange={this.onChangeEmail}
-                    />
-                  </div>     
-                  
+                    <Field type="text" class="form-control mr-sm-2 w-25" name="email" />
+                    {errors.email && touched.email ? (
+                      <div style={{ color: 'red' }}>{errors.email}</div>
+                    ) : null}
+                  </div>
+                  <div className="form-group mb-2">
+                    <label for="password" class="mr-sm-2 text-left d-block" >
+                      Password
+                    </label>
+                    <Field type="password" class="form-control mr-sm-2 w-25" name="password" />
+                    {errors.password && touched.password ? (
+                      <div style={{ color: 'red' }}>{errors.password}</div>
+                    ) : null}
+                  </div>
                   <div className="form-group">
                     <button type="submit" class="btn btn-primary mb-2">
                       Submit
                     </button>
                   </div>
-                </form>
+                </Form>
               </div>
             )}
         </Formik>
@@ -127,4 +125,3 @@ export default class userEdit extends Component {
     )
   }
 }
-
